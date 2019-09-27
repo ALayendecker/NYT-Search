8850a13c4c57b4efca58cc60eb17e7bd5121f9fb
ALayendecker <alcommits@gmail.com>
2019-09-27 12:39:20 -0500

    ajax base and query url

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,8 @@
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?" + article + "PXpYM6F0OjRxwTXHEOwko5iw6T4CsRMR"
-var article
 
 $("#search").on("click", function () {
-    var article = ;
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        person + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+var article = ;
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?" + article + "PXpYM6F0OjRxwTXHEOwko5iw6T4CsRMR"
+
 
 
     
