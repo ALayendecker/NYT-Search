89ed52adac6f10287a6e571653369a90f3ed6379
ALayendecker <alcommits@gmail.com>
2019-09-27 13:16:03 -0500

    setting up injest of search terms

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,18 @@
 
 $("#search").on("click", function () {
 var article = ;
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?" + article + "PXpYM6F0OjRxwTXHEOwko5iw6T4CsRMR"
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?" + query + articleLimit + startYear + endYear+ "&api-key=PXpYM6F0OjRxwTXHEOwko5iw6T4CsRMR"
+
+    var query = $("#search-term").val().trim()
+
+
+    //var articleLimit = $("#number-of-records").val().trim() 
+    // we are going to use a loop for article limits
+
+
+
+    var startYear = $("#start-year").val().trim()
+    var endYear = $("#end-year").val().trim()
 
 
 
@@ -12,4 +23,5 @@ $.ajax({
     method: "GET"
 })
     .then(function (response) {
+        console.log()
         var results = response.data;
